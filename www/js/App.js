7004a040119b062a94cf4ecd30cc45ac17ca08d0
Henning Leutz <leutz@pcsg.de>
2014-10-13 19:19:12 +0200

    qr code scan - test

--- www/js/App.js
+++ www/js/App.js
@@ -130,7 +130,33 @@ define([
                 this.resize();
                 this.Loader.hide();
             }).delay( 100, this )
+        },
+
+        /**
+         * Start the barcode scanner
+         */
+        scan : function()
+        {
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+            scanner.scan( function (result)
+            {
+
+                alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+                console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+
+
+            }, function (error)
+            {
+
+
+            });
         }
     });
-
 });
